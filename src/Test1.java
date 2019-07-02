43d11f25112a6098b872601e4cc5fdb82cf803ba
Yaroslav <Kaminski.y@mail.ru>
2019-07-02 12:13:14 +0300

    Initial commit

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+    public static void main(String[] args) {
+        int x = 10;
+
+        if (x == 10) {
+            int y = 20;
+            System.out.println("x and y: " + x + " " + y);
+            x = y * 2;
+        }
+           // y = 100;
+        System.out.println("x is " + x);
+    }
+}
